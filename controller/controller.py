5265f702007af949bf04f2018f71aff246c0c970
AgendaInaki <agendaInaki@gmail.com>
2025-09-19 19:32:03 -0300

    Update controller.py

---
+++ controller/controller.py
@@ -0,0 +1,25 @@
+import model
+
+def agregar_contacts(nombre, telefono, email):
+    return modelo.insert({
+        "name": nombre
+        "phone": telefono,
+        "email": email
+    })
+
+def lista_contacts():
+   return model.get_all()
+
+def buscar_contacts(nombre):
+   return model.search(nombre)
+
+def eliminar_contacts(nombre):
+   return model,delete(nombre)
+
+def actualizar_contacts(nombre, telefono=None, email=None):
+    nuevos_datos = {}
+    if telefono:
+        nuevos_datos["phone"] = telefono 
+    if email:
+        nuevos_datos["email"] = email
+    return model.update(nombre, nuevos_datos)
