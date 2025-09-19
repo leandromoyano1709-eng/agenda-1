edac2673f15f27b57c9a57ad48115bbc0633769b
Inaki842 <groltfil@gmail.com>
2025-09-19 13:20:09 +0000

    agregadas las funciones del modelo y creada la estructura de carpetas

--- model/model.py
+++ model/model.py
@@ -2,11 +2,20 @@ contacts = []
 
 def search(n:str):
     for c in contacts:
-        if i["name"]==n:
+        if c["name"]==n:
             return c
 
+    return None
+
 def insert(c):
-    pass
+    if search(c["name"]) is not None: return None
+    
+    contacts.append(c)
+    return c
 
 def delete(n:str):
-    pass
+    c = search(n)
+    if c is None: return None
+
+    contacts.remove(c)
+    return c
