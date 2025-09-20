0a4922316fb004055186fd25599fa24feb61be49
leandromoyano1709-eng <leandro.moyano1709@gmail.com>
2025-09-20 13:42:36 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from controller.controlador import ejecutar_opcion
+from view.vista import main_menu
+
+def main()
+    while true:
+        opcion = main_menu()
+        if opcion == "5":
+            print("¡gracias por usar la agenda!")
+            break 
+        ejecutar_opcion(opcion)
+
+  if __name__ == "__main__":
+      main()
