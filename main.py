8a60025323f38556c00da8f72ede84172cfdfd16
leandromoyano1709-eng <leandro.moyano1709@gmail.com>
2025-09-21 11:22:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from controller.controlador import ejecutar_opcion
 from view.vista import main_menu
 
-def main()
-    while true:
+def main():
+    while True:
         opcion = main_menu()
         if opcion == "5":
             print("¡gracias por usar la agenda!")
             break 
         ejecutar_opcion(opcion)
 
-  if __name__ == "__main__":
+if __name__ == "__main__":
       main()
